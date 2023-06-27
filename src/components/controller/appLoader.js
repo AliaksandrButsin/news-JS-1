a5ce8dc95b51aba09d9b1310a691c6a7ea8dcfae
AliaksandrButsin <aliaksandrbutsin@gmail.com>
2023-06-27 17:36:52 +0200

    fix: npm instal bug fix

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '87da412c6f5f44269f85b8ad39158ebc', // получите свой ключ https://newsapi.org/
         });
     }
 }
